ea920fd9a49986004fd85556c1e6df2c62de4016
Julissa749 <salinasjulissa6@gmail.com>
2020-11-27 22:59:17 -0600

    He agregado ProductoAdapter y RecyclerviewProductos

--- app/src/main/java/com/example/app_crud_mysql/ui/list_recycle_pro/ProductoAdapter.java
+++ app/src/main/java/com/example/app_crud_mysql/ui/list_recycle_pro/ProductoAdapter.java
@@ -29,6 +29,8 @@ public class ProductoAdapter extends RecyclerView.Adapter<ProductoAdapter.Produc
         return new ProductoHolder(vista);
     }
 
+
+
     @Override
     public void onBindViewHolder(ProductoHolder holder, int position) {
         dto_productos dto = listaProductos.get(position);
