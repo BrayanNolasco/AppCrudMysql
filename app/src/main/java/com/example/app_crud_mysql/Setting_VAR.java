e0443bd5ba8731de38c8b4015364a884dd320f09
CarlosAlfred20 <carlosalfredoalvarezlopez7001@gmail.com>
2020-11-29 17:38:37 -0600

    eh realizado el cambio setting_var

--- app/src/main/java/com/example/app_crud_mysql/Setting_VAR.java
+++ app/src/main/java/com/example/app_crud_mysql/Setting_VAR.java
@@ -1,40 +1,29 @@
 package com.example.app_crud_mysql;
 
-
 public class Setting_VAR {
-    public static final String TAG = "InicioActivity";
+ public static final String TAG = "InicioActivity";
 
+ public static final String mensajex="";
+ // Categoria                                              //Ingrese nueva Ip.//
+ public static final String URL_guardar_categoria = "http://192.168.0.9/service2020/guardar_categorias.php";
+ // public static final String URL_consultaAllCategorias = "http://192.168.0.9/service2020/buscar_Categorias.php";
+ public static final String URL_consultaCategoria_id = "http://192.168.0.9/service2020/buscar_categoria_id.php";
+ public static final String URL_update_categoria = "http://192.168.0.9/service2020/actualizar_categoria.php";
+ public static final String URL_delete_categoria = "http://192.168.0.9/service2020/eliminar_categoria.php";
+ public static final String URL_Login = "http://192.168.0.9/service2020/validar_usuarios.php";
+ public static final String URL_consultaAllCategorias = "http://192.168.0.9/service2020/buscar_Categorias.php";
+ //Producto
+ public static final String URL_registrar_productos = "http://192.168.0.9/service2020/guardar_producto.php";
+ public static final String URL_update_productos = "http://192.168.0.9/service2020/actualizar_producto.php";
+ public static final String URL_delete_productos = "http://192.168.0.9/service2020/eliminar_producto.php";
+ public static final String URL_consultarAllProductos = "http://192.168.0.9/service2020/buscarALLProductos.php";
+ public static final String URL_consultarAllProductos2 = "http://192.168.0.9/service2020/server/buscarALLProductos2.php";
 
-    public static final String mensajex="";
-
-
-// Categoria
-    public static final String URL_guardar_categoria = "https://appcrudmysql.000webhostapp.com/server/guardar_categorias.php";
-   // public static final String URL_consultaAllCategorias = "https://proyectorsfxresng.000webhostapp.com/service2020/buscar_Categorias.php";
-    public static final String URL_consultaCategoria_id = "https://appcrudmysql.000webhostapp.com/server/buscar_categoria_id.php";
-    public static final String URL_update_categoria = "https://appcrudmysql.000webhostapp.com/server/actualizar_categoria.php";
-    public static final String URL_delete_categoria = "https://appcrudmysql.000webhostapp.com/server/eliminar_categoria.php";
-    public static final String URL_Login = "https://appcrudmysql.000webhostapp.com/server/validar_usuarios.php";
-
-
-    public static final String URL_consultaAllCategorias = "https://appcrudmysql.000webhostapp.com/server/buscar_Categorias.php";
-
-    //Producto
-    public static final String URL_registrar_productos = "https://appcrudmysql.000webhostapp.com/server/guardar_producto.php";
-    public static final String URL_update_productos = "https://appcrudmysql.000webhostapp.com/server/actualizar_producto.php";
-    public static final String URL_delete_productos = "https://appcrudmysql.000webhostapp.com/server/eliminar_producto.php";
-    public static final String URL_consultarAllProductos = "https://appcrudmysql.000webhostapp.com/server/buscarALLProductos.php";
-    public static final String URL_consultarAllProductos2 = "https://appcrudmysql.000webhostapp.com/server/buscarALLProductos2.php";
-
-
-    //Usuario
-    public static final String URL_guardar_usuario = "https://appcrudmysql.000webhostapp.com/server/guardar_usuarios.php";
-    public static final String URL_update_usuario = "https://appcrudmysql.000webhostapp.com/server/actualizar_usuario.php";
-    public static final String URL_delete_usuario = "https://appcrudmysql.000webhostapp.com/server/eliminar_usuario.php";
-    public static final String URL_consultarAllUsuario = "https://appcrudmysql.000webhostapp.com/server/buscarALLusuario.php";
-    public static final String URL_consultarAllUsuario2 = "https://appcrudmysql.000webhostapp.com/server/buscarALLUsuario2.php";
-
-
-
+ //Usuario
+ public static final String URL_guardar_usuario = "http://192.168.0.9/service2020/guardar_usuarios.php";
+ public static final String URL_update_usuario = "http://192.168.0.9/service2020/actualizar_usuario.php";
+ public static final String URL_delete_usuario = "http://192.168.0.9/service2020/eliminar_usuario.php";
+ public static final String URL_consultarAllUsuario = "http://192.168.0.9/Sservice2020/buscarALLusuario.php";
+ public static final String URL_consultarAllUsuario2 = "http://192.168.0.9/service2020/buscarALLUsuario2.php";
 
 }
